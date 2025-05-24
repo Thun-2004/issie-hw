a1dc18020c98d4651f369a55398d1ed240bb5c92
Thun-2004 <66011276@kmitl.ac.th>
2025-05-24 22:50:37 +0700

    done rough structure

---
+++ react-hook-form/src/App.tsx
@@ -0,0 +1,237 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+const formSchema = z.object({
+  firstname: z.string().nonempty(),
+  lastname: z.string().nonempty(),
+  email: z.string().email(),
+  phone: z.string().length(10),
+  idnumber: z.string().length(13),
+  age: z.string().refine((val) => !Number.isNaN(parseInt(val, 10)), {
+    message: "Expected number, received a string",
+  }),
+  gender: z.enum(["Male", "Female", "Non-binary", "Prefer not to say"]),
+  country: z.enum(["Thailand", "Russia", "China", "North Korea"]),
+  birthdate: z.coerce.date().refine(
+    (date) => {
+      const today = new Date();
+      const minDate = new Date(
+        today.getFullYear() - 95,
+        today.getMonth(),
+        today.getDate()
+      );
+      const maxDate = new Date(
+        today.getFullYear() - 10,
+        today.getMonth(),
+        today.getDate()
+      );
+      return date >= minDate && date <= maxDate;
+    },
+    {
+      message: "Age must be between 10 and 95 years old",
+    }
+  ),
+  password: z.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/,
+  "Password must contain at least 1 uppercase,1 lowercase letter, and 1 number"),
+  confirmpassword: z.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/,
+  "Password must contain at least 1 uppercase,1 lowercase letter, and 1 number"),
+}).refine((data) => data.password === data.confirmpassword, {
+  message: "Passwords don't match",
+  path: ["confirmpassword"],
+});
+
+export default function ResponsiveForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      firstname: "",
+      lastname: "",
+      email: "",
+      phone: "",
+      idnumber: "",
+      age: "",
+      gender: "Male" as const,
+      country: "Thailand" as const,
+      birthdate: new Date("1930-01-01"),
+      password: "",
+      confirmpassword: "",
+    },
+  });
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
+  return (
+    <div className="py-12 flex items-center justify-center">
+      <div className="max-w-7xl w-full mx-auto p-4 sm:p-6">
+        <form className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div>
+            <label className="block pb-1">First Name</label>
+            <input
+              type="text"
+              {...register("firstname")}
+              placeholder="First Name"
+              className="border w-full rounded-md p-2 border-gray-400"
+            />
+            {errors.firstname && (
+              <p className="text-red-500">{errors.firstname.message}</p>
+            )}
+          </div>
+
+          <div>
+            <label className="block pb-1">Last Name</label>
+            <input
+              type="text"
+              {...register("lastname")}
+              placeholder="Last Name"
+              className="border w-full rounded-md p-2 border-gray-400"
+            />
+            {errors.lastname && (
+              <p className="text-red-500">{errors.lastname.message}</p>
+            )}
+          </div>
+
+          <div>
+            <label className="block pb-1">Email</label>
+            <input
+              type="email"
+              {...register("email")}
+              placeholder="Email"
+              className="border w-full rounded-md p-2 border-gray-400"
+            />
+            {errors.email && (
+              <p className="text-red-500">{errors.email.message}</p>
+            )}
+          </div>
+
+          <div>
+            <label className="block pb-1">Phone</label>
+            <input
+              type="tel"
+              {...register("phone")}
+              placeholder="Phone"
+              className="border w-full rounded-md p-2 border-gray-400"
+            />
+            {errors.phone && (
+              <p className="text-red-500">{errors.phone.message}</p>
+            )}
+          </div>
+
+          <div className="md:col-span-2">
+            <label className="block pb-1">ID number</label>
+            <input
+              type="text"
+              {...register("idnumber")}
+              placeholder="ID number"
+              className="border w-full rounded-md p-2 border-gray-400"
+            />
+            {errors.idnumber && (
+              <p className="text-red-500">{errors.idnumber.message}</p>
+            )}
+          </div>
+
+          <div>
+            <label className="block pb-1">Age</label>
+            <input
+              type="number"
+              {...register("age")}
+              placeholder="Age"
+              className="border w-full rounded-md p-2 border-gray-400"
+            />
+            {errors.age && <p className="text-red-500">{errors.age.message}</p>}
+          </div>
+
+          <div>
+            <label className="block pb-1">Gender</label>
+            <select
+              id=""
+              {...register("gender")}
+              className="border w-full rounded-md p-2 border-gray-400"
+            >
+              <option value="">-- Select Gender --</option>
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+              <option value="Non-binary">Non-binary</option>
+              <option value="Prefer not to say">Prefer not to say</option>
+            </select>
+            {errors.gender && (
+              <p className="text-red-500">{errors.gender.message}</p>
+            )}
+          </div>
+
+          <div>
+            <label className="block pb-1">Country</label>
+            <select
+              id=""
+              {...register("country")}
+              className="border w-full rounded-md p-2 border-gray-400"
+            >
+              <option value="">-- Select Country --</option>
+              <option value="Thailand">Thailand</option>
+              <option value="Russia">Russia</option>
+              <option value="China">China</option>
+              <option value="North Korea">North Korea</option>
+            </select>
+            {errors.country && (
+              <p className="text-red-500">{errors.country.message}</p>
+            )}
+          </div>
+
+          <div>
+            <label className="block pb-1">Birthdate</label>
+            <input
+              type="date"
+              {...register("birthdate")}
+              placeholder="Birthdate"
+              className="border w-full rounded-md p-2 border-gray-400"
+            />
+            {errors.birthdate && (
+              <p className="text-red-500">{errors.birthdate.message}</p>
+            )}
+          </div>
+
+          <div>
+            <label className="block pb-1">Password</label>
+            <input
+              type="password"
+              {...register("password")}
+              placeholder="Password"
+              className="border w-full rounded-md p-2 border-gray-400"
+            />
+            {errors.password && (
+              <p className="text-red-500">{errors.password.message}</p>
+            )}
+          </div>
+
+          <div>
+            <label className="block pb-1">Confirm Password</label>
+            <input
+              type="password"
+              {...register("confirmpassword")}
+              placeholder="Confirm Password"
+              className="border w-full rounded-md p-2 border-gray-400"
+            />
+            {errors.confirmpassword && (
+              <p className="text-red-500">{errors.confirmpassword.message}</p>
+            )}
+          </div>
+
+          <div className="md:col-span-2 pt-4">
+            <button
+              type="submit"
+              onClick={onSubmit}
+              className="bg-orange-600 text-white px-6 py-2 rounded-md hover:bg-orange-700"
+            >
+              Next
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
---
+++ react-hook-form/src/app2.tsx
@@ -0,0 +1,96 @@
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <div className="py-12 flex items-center justify-center">
+//       <div className="max-w-7xl w-full mx-auto p-4 sm:p-8">
+//         <form className="grid grid-cols-1 md:grid-cols-2 gap-4">
+//           <div>
+//               <label htmlFor="First Name" className="block pb-2">
+//                 First Name
+//               </label>
+//               <input type="text" className="border w-full rounded-md p-1 border-gray-400" placeholder=" First Name" />
+//           </div>
+
+//           <div>
+//               <label htmlFor="Last Name" className="block pb-2">
+//                 Last Name
+//               </label>
+//               <input type="text" className="border w-full rounded-md p-1 border-gray-400" placeholder=" Last Name" />
+//           </div>
+
+//           <div>
+//               <label htmlFor="First Name" className="block pb-2">
+//                 Email
+//               </label>
+//               <input type="text" className="border w-full rounded-md p-1 border-gray-400" placeholder=" Email" />
+//           </div>
+
+//           <div>
+//               <label htmlFor="Last Name" className="block pb-2">
+//                 Phone
+//               </label>
+//               <input type="text" className="border w-full rounded-md p-1 border-gray-400" placeholder=" Phone" />
+//           </div>
+
+//           <div className="grid col-span-2">
+//             <label htmlFor="Address" className="block pb-2">
+//                 Address
+//               </label>
+//               <input type="text" className="border w-full rounded-md p-1 border-gray-400" placeholder=" Address" />
+//           </div>
+          
+//           <div>
+//               <label htmlFor="Country" className="block pb-2">
+//                 Country
+//               </label>
+//               <input type="text" className="border w-full rounded-md p-1 border-gray-400" placeholder=" Country" />
+//           </div>
+
+//           <div>
+//               <label htmlFor="Province" className="block pb-2">
+//                 Province
+//               </label>
+//               <input type="text" className="border w-full rounded-md p-1 border-gray-400" placeholder=" Province" />
+//           </div>
+
+//           <div>
+//               <label htmlFor="City" className="block pb-2">
+//                 City
+//               </label>
+//               <input type="text" className="border w-full rounded-md p-1 border-gray-400" placeholder=" City" />
+//           </div>
+
+//           <div>
+//               <label htmlFor="Pincode" className="block pb-2">
+//                 Pincode
+//               </label>
+//               <input type="text" className="border w-full rounded-md p-1 border-gray-400" placeholder=" Pincode" />
+//           </div>
+
+//           <div>
+//               <label htmlFor="Password" className="block pb-2">
+//                 Password
+//               </label>
+//               <input type="text" className="border w-full rounded-md p-1 border-gray-400" placeholder=" Password" />
+//           </div>
+
+//           <div>
+//               <label htmlFor="confirm_password" className="block pb-2">
+//                 Confirm Password
+//               </label>
+//               <input type="text" className="border w-full rounded-md p-1 border-gray-400" placeholder=" Confirm Password" />
+//           </div>
+
+//           <div>
+//               <button type="submit" className="px-20 py-2 rounded-md bg-amber-600">Next</button>
+//           </div>
+          
+
+//         </form>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
